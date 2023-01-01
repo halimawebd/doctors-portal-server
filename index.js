4e8182328390c8c4a005674e9d49993e1e2c9f79
halimawebd <halimawebd@gmail.com>
2023-01-01 19:01:19 +0600

    users part

--- index.js
+++ index.js
@@ -211,20 +211,20 @@ try{
         res.send(users);
     });
 
-    // app.get('/users/admin/:email', async (req, res) => {
-    //     const email = req.params.email;
-    //     const query = { email }
-    //     const user = await usersCollection.findOne(query);
-    //     res.send({ isAdmin: user?.role === 'admin' });
+    app.get('/users/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { email }
+        const user = await usersCollection.findOne(query);
+        res.send({ isAdmin: user?.role === 'admin' });
        
-    // })
+    })
 
-    // app.post('/users', async(req, res) =>{
-    //     const user = req.body;
-    //     console.log(user);
-    //     const result = await usersCollection.insertOne(user);
-    //     res.send(result);
-    // });
+    app.post('/users', async(req, res) =>{
+        const user = req.body;
+        console.log(user);
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+    });
 
     // app.put('/users/admin/:id', veryfyJWT, veryfyAdmin, async(req, res) =>{
     //     const id = req.params.id;
